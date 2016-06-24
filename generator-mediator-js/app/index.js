0176fb7cc1f5a5b8404caac88f426a82f8010f1f
napiergit <andrew.napier@jembi.org>
2016-06-24 14:56:50 +0200

    OHM-239: Add meaningul config properties and update to work with latest yeoman

--- generator-mediator-js/app/index.js
+++ generator-mediator-js/app/index.js
@@ -1,120 +1,115 @@
-'use strict';
+'use strict'
 
-var util = require('util');
-var path = require('path');
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var uuid = require('node-uuid');
-var optionOrPrompt = require('yeoman-option-or-prompt');
+var generator = require('yeoman-generator')
+var chalk = require('chalk')
+var yosay = require('yosay')
+var uuid = require('node-uuid')
 
-var mediatorJsGenerator = yeoman.generators.Base.extend({
-  
-  _optionOrPrompt: optionOrPrompt,
+var prompts = require('./prompts')
+var config = require('./config')
+
+var mediatorJsGenerator = generator.Base.extend({
+  constructor: function () {
+    generator.Base.apply(this, arguments)
+
+    this.option('useDefaults')
+    this.enablePrompts = (this.options.useDefaults ? false : true)
+  },
   
   prompting: function () {
-    var done = this.async();
-
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the marvelous ' + chalk.red('MediatorJs') + ' generator!'
-    ));
-
-    var prompts = [{
-        type: 'input', 
-        name: 'mediatorName', 
-        message: 'What is your Mediator\'s name?', 
-        default: 'Yeoman Generated Mediator',
-        validate: function(mediatorName){ 
-          if(mediatorName !== ''){ return true; }else{ return 'Please supply a Mediator Name'; } 
-        } 
-      }, { 
-        type: 'input', 
-        name: 'mediatorDesc', 
-        message: 'What does your Mediator do?',
-        default: 'Brief Description'
-      }, { 
-        type: 'input', 
-        name: 'configPort', 
-        message: 'Under what port number should the mediator run?', 
-        default: 3000 
-      }, { 
-        type: 'input', 
-        name: 'mediatorRoutePath', 
-        message: 'What is your primary route path?',
-        default: 'primary/path/'
-      }, { 
-        name: 'enablePPA',
-        type: 'confirm',
-        message: 'Would you like to package your app?',
-        default: false
-      }, {
-        when: function (response) {
-          return response.enablePPA;
-        },
-        name: 'good-or-not',
-        message: 'Sweet! Was it any good?'
-      }];
-
-    this._optionOrPrompt(prompts, function (props) {
+    return this.prompt(prompts.promptsList()).then(function (props) {
       // Mediator settings
-      this.configPort = props.configPort;
-      this.mediatorName = props.mediatorName;
-      this.mediatorDesc = props.mediatorDesc;
-      this.mediatorRoutePath = props.mediatorRoutePath;
+      this.mediatorName = props.mediatorName || config.mediatorName
+      this.mediatorDesc = props.mediatorDesc || config.mediatorDesc
+      this.mediatorAuthor = props.mediatorAuthor || config.mediatorAuthor
+      this.mediatorMaintainer = props.mediatorMaintainer || config.mediatorMaintainer
+      this.configPort = props.configPort || config.configPort
+      this.defaultChannelPath = props.defaultChannelPath || config.defaultChannelPath
+      this.mediatorRoutePath = props.mediatorRoutePath || config.mediatorRoutePath
+      this.mediatorRouteHost = props.mediatorRouteHost || config.mediatorRouteHost
+      
+      // API settings
+      this.configureApi = props.configureApi || config.configureApi
+      this.mediatorApiUsername = props.mediatorApiUsername || config.mediatorApiUsername
+      this.mediatorApiPassword = props.mediatorApiPassword || config.mediatorApiPassword
+      this.mediatorApiUrl = props.mediatorApiUrl || config.mediatorApiUrl
+      this.mediatorApiSsl = props.mediatorApiSsl || config.mediatorApiSsl
+      this.mediatorRegister = props.mediatorRegister || config.mediatorRegister
       
       // PPA settings
-      this.enablePPA = props.enablePPA;
-      
-      done();
-    }.bind(this));
-  },
-    
-  scaffoldFolders: function() {
-    this.mkdir("config/");
-    this.mkdir("lib/");
-    this.mkdir("test/");
-    
-    if(this.enablePPA) {
-      this.mkdir("packaging/")
-    }
+      this.enablePPA = props.enablePPA || config.enablePPA
+      this.ppaUsername = props.ppaUsername || config.ppaUsername
+    }.bind(this))
   },
 
-  copyMainFiles: function() {
-
-    var context = { 
+  writing: function() {
+    // Set up context objects to populate templates
+    var mediatorContext = { 
       configPort: this.configPort,
-      mediatorUUID: "urn:uuid:"+uuid.v1(),
+      mediatorUUID: "urn:uuid:" + uuid.v1(),
       appName: this.mediatorName.replace(/ /g,"-"),
       mediatorName: this.mediatorName,
       mediatorDesc: this.mediatorDesc,
-      mediatorRoutePath: this.mediatorRoutePath,
-    };
-
-    this.template("_gruntfile.js", "Gruntfile.js", context);
-    this.template("_package.json", "package.json", context);
-    this.template("_mediator.json", "config/mediator.json", context);
-
-    this.copy("_config.json", "config/config.json");
-    this.copy("_index.js", "lib/index.js");
-    this.copy("_register.js", "lib/register.js");
+      mediatorAuthor: this.mediatorAuthor,
+      mediatorMaintainer: this.mediatorMaintainer,
+      defaultChannelPath: this.defaultChannelPath,
+      mediatorRouteHost: this.mediatorRouteHost,
+      mediatorRoutePath: this.mediatorRoutePath
+    }
+    
+    var apiContext = {
+      mediatorApiUsername: this.mediatorApiUsername,
+      mediatorApiPassword: this.mediatorApiPassword,
+      mediatorApiUrl: this.mediatorApiUrl,
+      mediatorApiSsl: this.mediatorApiSsl,
+      mediatorRegister: this.mediatorRegister
+    }
+    
+    var ppaContext = {
+      ppaUsername: this.ppaUsername,
+      mediatorName: this.mediatorName,
+      appName: this.mediatorName.replace(/ /g,"-"),
+      mediatorMaintainer: this.mediatorMaintainer,
+      mediatorDesc: this.mediatorDesc
+    }
+    
+    // Add the mediator files
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/_package.json'), this.destinationPath('package.json'), mediatorContext)
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/config/_mediator.json'), this.destinationPath('config/mediator.json'), mediatorContext)
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/config/_config.json'), this.destinationPath('config/config.json'), apiContext)
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/lib/_index.js'), this.destinationPath('lib/index.js'))
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/lib/_openhim.js'), this.destinationPath('lib/openhim.js'))
+    this.fs.copyTpl(this.templatePath('mediatorTemplate/lib/_utils.js'), this.destinationPath('lib/utils.js'))
+    
+    // Add the PPA files
+    this.fs.copyTpl(this.templatePath('packagingTemplate/_.dput.cf'), this.destinationPath('packaging/.dput.cf'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/_cp-mediators-into-pkg.sh'), this.destinationPath('packaging/cp-mediators-into-pkg.sh'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/_create-deb.sh'), this.destinationPath('packaging/create-deb.sh'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_changelog'), this.destinationPath('packaging/targets/trusty/debian/changelog'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_control'), this.destinationPath('packaging/targets/trusty/debian/control'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_postinst'), this.destinationPath('packaging/targets/trusty/debian/postinst'), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/etc/init/_mediator.conf'), this.destinationPath('packaging/targets/trusty/etc/init/' + ppaContext.appName), ppaContext)
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_config'), this.destinationPath('packaging/targets/trusty/debian/config'))
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_install'), this.destinationPath('packaging/targets/trusty/debian/install'))
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_rules'), this.destinationPath('packaging/targets/trusty/debian/rules'))
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/debian/_templates'), this.destinationPath('packaging/targets/trusty/debian/templates'))
+    this.fs.copyTpl(this.templatePath('packagingTemplate/targets/trusty/etc/openhim/_install-node-4.sh'), this.destinationPath('packaging/targets/trusty/etc/openhim/install-node-4.sh'))
   },
 
   install: function () {
     this.installDependencies({
-      npm:true,
-      bower:false,
-      skipInstall: this.options['skip-install'],
+      npm: true,
+      bower: false,
       callback: function () {
         // On successful install, have Yeoman greet the user.
         console.log(yosay(
           'Scaffolding Complete!\r' + 
-          chalk.green('Remember: "npm start"\r') + 
+          chalk.green('Simply run: "npm start"\r') + 
           'Goodbye!'
-        ));
+        ))
       }
-    });
+    })
   }
-});
+})
 
-module.exports = mediatorJsGenerator;
+module.exports = mediatorJsGenerator
